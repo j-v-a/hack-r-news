5da8c1e7356f7f7153ead90ad9ac15b680b8bde8
j-v-a <jvanameijden@gmail.com>
2018-08-08 14:10:10 +0200

    Add error handling

--- src/App.js
+++ src/App.js
@@ -32,7 +32,8 @@ class App extends Component {
 		this.state = {
 			results: null,
 			searchKey: '',
-			searchTerm: DEFAULT_QUERY
+			searchTerm: DEFAULT_QUERY,
+			error: null
 		};
 
 		this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
@@ -72,7 +73,7 @@ class App extends Component {
 			.then(result => {
 				this.setSearchTopStories(result);
 			})
-			.catch(error => error);
+			.catch(error => this.setState({ error }));
 	}
 
 	componentDidMount() {
@@ -112,11 +113,15 @@ class App extends Component {
 	}
 
 	render() {
-		const { searchTerm, results, searchKey } = this.state;
+		const { searchTerm, results, searchKey, error } = this.state;
 		const page =
 			(results && results[searchKey] && results[searchKey].page) || 0;
 		const list =
 			(results && results[searchKey] && results[searchKey].hits) || [];
+
+		if (error) {
+			return <p>Something went wrong.</p>;
+		}
 
 		return (
 			<div className="Page">
@@ -129,7 +134,13 @@ class App extends Component {
 						Search
 					</Search>
 				</div>
-				<Table list={list} onDismiss={this.onDismiss} />
+				{error ? (
+					<div className="interactions">
+						<p>Something went wrong.</p>
+					</div>
+				) : (
+					<Table list={list} onDismiss={this.onDismiss} />
+				)}
 				<div className="interactions">
 					<Button
 						onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
