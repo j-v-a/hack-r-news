8788a7217a4110ae97027d08c47a5a8e12d2f904
j-v-a <jvanameijden@gmail.com>
2018-08-13 10:46:04 +0200

    Add search focus on load

--- src/App.js
+++ src/App.js
@@ -176,12 +176,30 @@ Button.propTypes = {
 	children: PropTypes.node.isRequired
 };
 
-const Search = ({ value, onChange, onSubmit, children }) => (
-	<form onSubmit={onSubmit}>
-		<input type="text" value={value} onChange={onChange} />
-		<button type="submit">{children}</button>
-	</form>
-);
+class Search extends Component {
+	componentDidMount() {
+		if (this.input) {
+			this.input.focus();
+		}
+	}
+
+	render() {
+		const { value, onChange, onSubmit, children } = this.props;
+		return (
+			<form onSubmit={onSubmit}>
+				<input
+					type="text"
+					value={value}
+					onChange={onChange}
+					ref={node => {
+						this.input = node;
+					}}
+				/>
+				<button type="submit">{children}</button>
+			</form>
+		);
+	}
+}
 
 Search.propTypes = {
 	value: PropTypes.string,
